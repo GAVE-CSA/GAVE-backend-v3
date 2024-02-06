51ea617849eddfaffa1ce12d5c0f2efb97965e1e
rachit-j <rjaiswal.sd.77@gmail.com>
2024-02-06 10:03:58 -0800

    remove one cors

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -24,10 +24,10 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
     }
 
-    
+    /*
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("https://gave-csa.github.io");
     }
-    
+    */
 }
