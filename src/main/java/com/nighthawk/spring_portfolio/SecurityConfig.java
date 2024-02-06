2638d49561367e47811fc64c9667c20e03a76b3d
rachit-j <rjaiswal.sd.77@gmail.com>
2024-02-06 00:09:39 -0800

    Revert "Merge pull request #5 from GAVE-CSA/emma"
    This reverts commit a4d5eefac9d20f664bb60377453615d1c2660e1a, reversing
    changes made to 991b38190182ba85734bcc5b19b44489af3e44ad.

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
 				// support cors
 				.cors(Customizer.withDefaults())
 				.headers(headers -> headers
-					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "http://127.0.0.1:4100"))
 					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
 					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
 					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
